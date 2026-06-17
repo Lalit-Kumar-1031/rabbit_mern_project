f80c8733d688c1e3991f19bd6ef06889ae76c128
Lalit Kumar <lalitkumar1031@gmail.com>
2026-06-17 21:41:05 +0530

    13:27H

---
+++ frontend/vite-project/src/redux/slices/adminProductSlice.js
@@ -0,0 +1,155 @@
+import { create, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const Base_Url = import.meta.env.VITE_BACKEND_URL;
+
+//Async Thunk for Fetch Admin Products
+export const fetchAdminProducts = createAsyncThunk(
+  "adminProducts/fetchProducts",
+  async ({ rejectWithValue }) => {
+    try {
+      const response = await axios.get(`${Base_Url}/api/admin/products`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+//Async Thunk to Create new Product
+export const createProduct = createAsyncThunk(
+  "adminProducts/createProduct",
+  async ({ productData }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(
+        `${Base_Url}/api/admin/products`,
+        productData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Async Thunk to update admin Product
+export const updateProduct = createAsyncThunk(
+  "adminProducts/updateProduct",
+  async ({ id, productData }, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(
+        `${Base_Url}/api/admin/products/${id}`,
+        productData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Async Thunk to delete Product
+export const deleteProduct = createAsyncThunk(
+  "adminProducts/deleteProduct",
+  async (id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`${Base_Url}/api/admin/products/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      return id;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+// Create Slice
+const adminProductsSlice = createSlice({
+  name: "adminProducts",
+  initialState: {
+    products: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    //Fetch Admin Products
+    builder
+      .addCase(fetchAdminProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchAdminProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(fetchAdminProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      //Create Product
+      .addCase(createProduct.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createProduct.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products.push(action.payload);
+      })
+      .addCase(createProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      //Update Product
+      .addCase(updateProduct.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateProduct.fulfilled, (state, action) => {
+        state.loading = false;
+        const productIndex = state.products.findIndex(
+          (product) => product._id === action.payload._id,
+        );
+        if (productIndex !== -1) {
+          state.products[productIndex] = action.payload;
+        }
+      })
+      .addCase(updateProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      //Delete Product
+      .addCase(deleteProduct.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteProduct.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload,
+        );
+      })
+      .addCase(deleteProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default adminProductsSlice.reducer;
--- frontend/vite-project/src/redux/slices/adminSlice.js
+++ frontend/vite-project/src/redux/slices/adminSlice.js
@@ -33,3 +33,115 @@ export const addUser = createAsyncThunk(
     }
   },
 );
+
+//Async Thunk to update the admin user
+export const updateUser = createAsyncThunk(
+  "admin/updateUser",
+  async ({ id, name, email, role }, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(
+        `${Base_Url}/api/admin/users/${id}`,
+        { name, email, role },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+          },
+        },
+      );
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+//Async Thunk to delete user
+export const deleteUser = createAsyncThunk(
+  "admin/deleteUser",
+  async (id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`${Base_Url}/api/admin/users/${id}`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+        },
+      });
+      return id;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+//Create admin Slice
+const adminSlice = createSlice({
+  name: "admin",
+  initialState: {
+    users: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    //Fetch Admin Users
+    builder
+      .addCase(fetchUsers.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      //Update admin User
+      .addCase(updateUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateUser.fulfilled, (state, action) => {
+        state.loading = false;
+        const updatedUser = action.payload;
+        const userIndex = state.users.findIndex(
+          (user) => user._id === updatedUser._id,
+        );
+        if (userIndex !== -1) {
+          state.users[userIndex] = updatedUser;
+        }
+      })
+      .addCase(updateUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      // delete user
+      .addCase(deleteUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteUser.fulfilled, (state, action) => {
+        state.loading = false;
+        const deletedUserId = action.payload;
+        state.users = state.users.filter((user) => user._id !== deletedUserId);
+      })
+      .addCase(deleteUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      }) // Add New user
+      .addCase(addUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.users.push(action.payload.user);
+      })
+      .addCase(addUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload.message;
+      });
+  },
+});
+
+export default adminSlice.reducer;
